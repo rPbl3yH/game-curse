7f9a8a280e23bbe88dcd53f73a183b718366b3af
rPbl3yH <tnv2205@gmail.com>
2024-06-07 19:22:10 +0300

    fix: rect distance sensor

--- Assets/Game/Scripts/Gameplay/Wall/RectangleDistanceSensor.cs
+++ Assets/Game/Scripts/Gameplay/Wall/RectangleDistanceSensor.cs
@@ -7,11 +7,12 @@ namespace Game.Scripts.Gameplay.Wall
 {
     public class RectangleDistanceSensor : MonoBehaviour
     {
+        [SerializeField] private Transform _centerPoint;
         [SerializeField] private Transform _targetTransform; 
         [SerializeField] private Vector2 _rectangleSize; 
 
         [ShowInInspector, ReadOnly]
-        private bool _isInRectangle = false; 
+        private bool _isInRectangle; 
 
         public event Action TargetEntered; 
         public event Action TargetExited; 
@@ -23,15 +24,15 @@ namespace Game.Scripts.Gameplay.Wall
 
         void CheckPositionInRectangle()
         {
-            float xCheck = Mathf.Abs(_targetTransform.position.x - transform.position.x) / _rectangleSize.x;
-            float yCheck = Mathf.Abs(_targetTransform.position.y - transform.position.y) / _rectangleSize.y;
+            float xCheck = Mathf.Abs(_targetTransform.position.x - _centerPoint.position.x) / _rectangleSize.x;
+            float zCheck = Mathf.Abs(_targetTransform.position.z - _centerPoint.position.z) / _rectangleSize.y;
 
-            if ((xCheck > 1 || yCheck > 1) && _isInRectangle)
+            if ((xCheck > 1 || zCheck > 1) && _isInRectangle)
             {
                 TargetExited?.Invoke();
                 _isInRectangle = false;
             }
-            else if (xCheck <= 1 && yCheck <= 1 && _isInRectangle == false)
+            else if (xCheck <= 1 && zCheck <= 1 && !_isInRectangle)
             {
                 TargetEntered?.Invoke();
                 _isInRectangle = true;
@@ -42,7 +43,7 @@ namespace Game.Scripts.Gameplay.Wall
         {
             Handles.color = Color.black;
             var size = new Vector3(_rectangleSize.x, 0.01f, _rectangleSize.y);
-            Handles.DrawWireCube(transform.position, size);
+            Handles.DrawWireCube(_centerPoint.position, size);
         }
     }
 }
